d929bb23ad9bd063b517a0a734eb1502f5e1a593
Your Name <your-email@example.com>
2025-12-12 18:38:08 +0500

    user form

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// connect mongo
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB CONNECTED"))
+  .catch(err => console.log(err));
+
+// Schema
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+// route
+app.post("/create-user", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.json({ message: "User saved successfully!", user: newUser });
+  } catch (error) {
+    res.status(500).json({ message: "Error saving user", error });
+  }
+});
+
+app.listen(7000, () => console.log("Server running on port 7000"));
